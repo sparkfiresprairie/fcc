b6b56e591676afb4bff21156b1302e7419e71fe8
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2017-03-03 00:24:37 -0600

    add click event

--- front_end/random_quote_machine/random_quote_machine.js
+++ front_end/random_quote_machine/random_quote_machine.js
@@ -1,8 +1,13 @@
 /**
  * Created by Xingyuan on 3/2/17.
  */
-$(document).ready(function() {
-    $("#new-quote-btn").on("click", function () {
-        $("#quote").html("Here is the message");
+function getQuote() {
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function (json) {
+        $("#quote").html(json[0].content);
+        $("#cite").html(json[0].title);
     });
+}
+
+$(document).ready(function () {
+    $("#new-quote-btn").on("click", getQuote);
 });
