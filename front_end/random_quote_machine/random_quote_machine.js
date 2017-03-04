e6c5f8f68251d66ab286a79ba3a83f4c94671583
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2017-03-04 00:02:54 -0600

    realize basic functionality: get quote and tweet quote

--- front_end/random_quote_machine/random_quote_machine.js
+++ front_end/random_quote_machine/random_quote_machine.js
@@ -1,13 +1,44 @@
 /**
  * Created by Xingyuan on 3/2/17.
  */
-function getQuote() {
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function (json) {
-        $("#quote").html(json[0].content);
-        $("#cite").html(json[0].title);
-    });
-}
 
-$(document).ready(function () {
+// https://market.mashape.com/andruxnet/random-famous-quotes
+function getQuote() {
+    $.ajax({
+        headers: {
+            "X-Mashape-Key": "UsONESE3zzmshM3O3v19H6pGnONap1o91rKjsnxaWbKeqBRuUM",
+            "Content-Type": "application/x-www-form-urlencoded",
+            Accept: "application/json"
+        },
+        url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+        success: function (response) {
+            var r = JSON.parse(response);
+            currentQuote = r.quote;
+            currentAuthor = r.author;
+            $("#quote-author").html(currentAuthor);
+            $("#quote-text").html(currentQuote);
+        },
+        cache: false
+    });
+};
+
+function inIframe () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+};
+
+// https://dev.twitter.com/web/tweet-button/web-intent
+function tweetQuote() {
+    if (!inIframe()) {
+        window.open("https://twitter.com/intent/tweet?hashtags=RandomQuoteMachine&text=" + encodeURI('"' + currentQuote + '"' + " - " + currentAuthor));
+    }
+};
+
+$(document).ready(function() {
+    getQuote();
     $("#new-quote-btn").on("click", getQuote);
+    $("#tweet-quote-btn").on("click", tweetQuote);
 });
